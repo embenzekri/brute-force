5c4e09113ccb266c0f4bce09660cf7cc294a7a82
EL MAHDI BENZEKRI <elmehdi.benzekri@nestle.com>
2021-03-07 21:26:06 +0100

    Create UT

---
+++ src/main/java/ma/sqli/brute/force/BruteForceApp.java
@@ -0,0 +1,20 @@
+package ma.sqli.brute.force;
+
+/**
+ * @author : El Mahdi Benzekri
+ * @since : 3/7/21, dim.
+ **/
+public class BruteForceApp {
+
+    public String login(String username, String password) {
+        return "";
+    }
+
+    public void addUser(String username, String password) {
+
+    }
+
+    public String loginWithAndroid(String username, String password) {
+        return "";
+    }
+}
