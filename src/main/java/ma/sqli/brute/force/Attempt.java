4bd274c180ff54493800d1aac2eb0412e6b5284f
OmaimaEl-youssefy <omaimaelyo@gmail.com>
2022-03-21 16:04:57 +0100

    first commit - not working

---
+++ src/main/java/ma/sqli/brute/force/Attempt.java
@@ -0,0 +1,17 @@
+package ma.sqli.brute.force;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Attempt {
+
+    private User user;
+    List<String> usersLoggedIn = new ArrayList<>();
+
+    public Attempt(User user) {
+        this.user = user;
+    }
+
+
+
+}
--- src/main/java/ma/sqli/brute/force/BruteForceApp.java
+++ src/main/java/ma/sqli/brute/force/BruteForceApp.java
@@ -1,20 +1,43 @@
 package ma.sqli.brute.force;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * @author : El Mahdi Benzekri
  * @since : 3/7/21, dim.
  **/
 public class BruteForceApp {
 
+    Map<String, String> AddedUsers = new HashMap<>();
+    Map<String, String> loggedInUsers = new HashMap<>();
+    String result = "";
+
     public String login(String username, String password) {
-        return "";
+
+        if (AddedUsers.get(username).equals(password)) {
+            result = "Welcome " + username + "!";
+            loggedInUsers.remove(username);
+        } else if (loggedInUsers.containsKey(username)) {
+            result = "Multiple erroneous credentials, your account is locked.";
+        } else {
+            result = "User or password are incorrect.";
+        }
+
+        loggedInUsers.put(username, password);
+
+        return result;
     }
 
     public void addUser(String username, String password) {
-
+        AddedUsers.put(username, password);
     }
 
     public String loginWithAndroid(String username, String password) {
         return "";
+    }
+
+    public void blacklist(String sqli) {
+
     }
 }
