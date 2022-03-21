1118d30f01d2f5295316a3e980c63357a6fa8bdd
belhadj-achraf <belhadjachraf6@gmail.com>
2022-03-21 16:04:25 +0100

    success

--- src/main/java/ma/sqli/brute/force/BruteForceApp.java
+++ src/main/java/ma/sqli/brute/force/BruteForceApp.java
@@ -9,15 +9,10 @@ import java.util.LinkedList;
  **/
 public class BruteForceApp {
 
-    private final String USER_NAME_ADMIN = "admin";
-    private final String PASSWORD_ADMIN = "123";
-    private final String USER_NAME_SQLI = "sqli";
-    private final String PASSWORD_SQlI = "0000";
-    HashMap<String, Integer> lockedList = new HashMap<String, Integer>();
-    HashMap<String, String> userList = new HashMap<String, String>();
+    private static HashMap<String, Integer> lockedList = new HashMap<>();
+    private static HashMap<String, String> userList = new HashMap<>();
 
     public String login(String username, String password) {
-
         if (userList.containsKey(username) && userList.get(username) == password) {
             return "Welcome " + username + "!";
         } else {
@@ -26,20 +21,13 @@ public class BruteForceApp {
     }
 
     public void addUser(String username, String password) {
-        for (int i = 0; i < userList.size(); i++) {
-            if (!userList.containsKey(username) && password.length() > 2) {
-                userList.put(username, password);
-            }
+        if (!userList.containsKey(username) && password.length() > 2) {
+            userList.put(username, password);
         }
     }
 
     public String loginWithAndroid(String username, String password) {
-        if (USER_NAME_SQLI == username && PASSWORD_SQlI == password ||
-            USER_NAME_SQLI == username && PASSWORD_SQlI == password) {
-            return "Welcome " + username + "!";
-        } else {
-            return "User or password are incorrect.";
-        }
+        return "";
     }
 
     public void blacklist(String sqli) {
