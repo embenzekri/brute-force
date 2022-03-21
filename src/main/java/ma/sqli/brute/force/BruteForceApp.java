1334ff082322ea4c84f8ff3c40962ccfe8fbd7b8
Halimaelc <halimaelcadi2@gmail.com>
2022-03-21 16:21:29 +0100

    still needs work

--- src/main/java/ma/sqli/brute/force/BruteForceApp.java
+++ src/main/java/ma/sqli/brute/force/BruteForceApp.java
@@ -15,13 +15,14 @@ public class BruteForceApp {
     private Compte compte;
 
     public String login(String username, String password) {
+        /*if (User.containsValue(username) == false || User.get(username) != password) {
+            result= "User or password are incorrect.";
+        }*/
 
-        String wrong = WrongCredentials(username, password);
-        result = blacklist(username);
+        result=blacklist(username);
         if (password.length() < 2) {
             result = "Your password is too weak, please update it by going to your my account.";
         }
-
         return result;
     }
 
@@ -35,18 +36,20 @@ public class BruteForceApp {
     }
 
     public String blacklist(String username) {
-        String result = "Welcome " + username + "!";
+        result = "Welcome " + username + "!";
         if (username == "sqli") {
             result = "Your account is blacklisted, contact the CRC to resolve the problem.";
         }
         return result;
     }
 
-    public String WrongCredentials(String username, String password) {
-        String result = "Welcome " + username + "!";
+   /* public String WrongCredentials(String username, String password) {
         if (User.containsValue(username) == true && User.get(username) == password) {
-            result = "User or password are incorrect.";
+            result = "Welcome " + username + "!";
+        }
+        else{
+             result = "User or password are incorrect.";
         }
         return result;
-    }
+    }*/
 }
