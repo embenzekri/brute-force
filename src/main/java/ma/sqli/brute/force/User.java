26d6df1e00d275dff6c375afc4d84e162fdcfbf5
Anir Elmoussaid <anir.elmoussaid@gmail.com>
2022-03-21 15:40:59 +0100

    Implemented User & UserDatabase

---
+++ src/main/java/ma/sqli/brute/force/User.java
@@ -0,0 +1,22 @@
+package ma.sqli.brute.force;
+
+public class User {
+    private String username;
+    private String password;
+
+    public User(String username, String password){
+        this.username = username;
+        this.password = password;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+
+
+}
