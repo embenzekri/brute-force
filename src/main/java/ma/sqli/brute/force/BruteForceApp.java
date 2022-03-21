4234196b44e06590a34f39ef5e6f94b50bf3610a
EL MAHDI BENZEKRI <elmehdi.benzekri@nestle.com>
2022-03-21 17:37:35 +0100

    Implement single device login

--- src/main/java/ma/sqli/brute/force/BruteForceApp.java
+++ src/main/java/ma/sqli/brute/force/BruteForceApp.java
@@ -1,20 +1,68 @@
 package ma.sqli.brute.force;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * @author : El Mahdi Benzekri
  * @since : 3/7/21, dim.
  **/
 public class BruteForceApp {
 
+    private Map<String, String> usernameAndPasswords = new HashMap<>();
+
+    private Map<Device, Map<String, Integer>> LoginErrorsByDevice = new HashMap<>();
+    private Map<String, Integer> loginErrorsByUsername = new HashMap<>();
+    private Map<String, Integer> loginErrorsByUsernameAndroid = new HashMap<>();
+
     public String login(String username, String password) {
-        return "";
-    }
-
-    public void addUser(String username, String password) {
-
+        if (isValidCredentials(username, password)) {
+            resetErrorsCount(username, loginErrorsByUsername);
+            return String.format("Welcome %s!", username);
+        } else if (loginErrorsByUsername.get(username) >= 2) {
+            return "Multiple erroneous credentials, your account is locked.";
+        } else {
+            incrementErrorsCount(username, loginErrorsByUsername);
+            return "User or password are incorrect.";
+        }
     }
 
     public String loginWithAndroid(String username, String password) {
-        return "";
+        if (isValidCredentials(username, password)) {
+            resetErrorsCount(username, loginErrorsByUsernameAndroid);
+            return String.format("Welcome %s!", username);
+        } else if (loginErrorsByUsernameAndroid.get(username) >= 2) {
+            return "Multiple erroneous credentials, your account is locked.";
+        } else {
+            incrementErrorsCount(username, loginErrorsByUsernameAndroid);
+            return "User or password are incorrect.";
+        }
+    }
+
+    private void resetErrorsCount(String username, Map<String, Integer> loginErrorsByUsername) {
+        loginErrorsByUsername.put(username, 0);
+    }
+
+    private void incrementErrorsCount(String username, Map<String, Integer> loginErrorsByUsername) {
+        loginErrorsByUsername.put(username, loginErrorsByUsername.get(username) + 1);
+    }
+
+    private boolean isValidCredentials(String username, String password) {
+        return usernameAndPasswords.containsKey(username) &&
+                usernameAndPasswords.get(username).equals(password);
+    }
+
+    public void addUser(String username, String password) {
+        usernameAndPasswords.put(username, password);
+        if (!loginErrorsByUsername.containsKey(username)) {
+            resetErrorsCount(username, loginErrorsByUsername);
+        }
+        if (!loginErrorsByUsernameAndroid.containsKey(username)) {
+            resetErrorsCount(username, loginErrorsByUsernameAndroid);
+        }
+    }
+
+    public void blacklist(String sqli) {
+
     }
 }
---
+++ src/main/java/ma/sqli/brute/force/Device.java
@@ -0,0 +1,9 @@
+package ma.sqli.brute.force;
+
+/**
+ * @author : El Mahdi Benzekri
+ * @since : 21/03/2022, lun.
+ **/
+public enum Device {
+    WEB, ANDROID
+}
