5fe186ff6c257bd0ea29e7274e5981db729e45ea
hafid-laasri <hafid.laasri1234@gmail.com>
2022-03-22 08:49:21 +0100

    un comit

--- src/main/java/ma/sqli/brute/force/BruteForceApp.java
+++ src/main/java/ma/sqli/brute/force/BruteForceApp.java
@@ -1,20 +1,47 @@
 package ma.sqli.brute.force;
 
-/**
- * @author : El Mahdi Benzekri
- * @since : 3/7/21, dim.
- **/
+import java.util.HashMap;
+import java.util.Map;
+
 public class BruteForceApp {
 
+    Map<String,String> users = new HashMap<>();
+    Map<String,Integer> attemps = new HashMap<>();
+    Map<String,Integer> attemptsAndroid = new HashMap<>();
     public String login(String username, String password) {
+        if (users.containsKey(username)&& users.get(username).equals(password)){
+            attemps.put(username,0);
+            return "Welcome "+username+"!";
+        }else if (users.get(username)!=password){
+            attemps.put(username,attemps.get(username)+1);
+            if (attemps.get(username)>2){
+                return "Multiple erroneous credentials, your account is locked.";
+            }
+            return "User or password are incorrect.";
+        }
         return "";
     }
 
     public void addUser(String username, String password) {
-
+        users.put(username,password);
+        attemps.put(username,0);
+        attemptsAndroid.put(username,0);
     }
 
     public String loginWithAndroid(String username, String password) {
+        if (users.containsKey(username)&& users.get(username).equals(password)){
+            attemptsAndroid.put(username,0);
+            return "Welcome "+username+"!";
+        }else if (users.get(username)!=password){
+            attemptsAndroid.put(username,attemptsAndroid.get(username)+1);
+            if (attemptsAndroid.get(username)>2){
+                return "Multiple erroneous credentials, your account is locked.";
+            }
+            return "User or password are incorrect.";
+        }
         return "";
+    }
+
+    public void blacklist(String sqli) {
     }
 }
