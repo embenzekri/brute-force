7fb09bc7503e63a6594779f2053e7de708536bd4
belhadj-achraf <belhadjachraf6@gmail.com>
2022-03-22 09:39:50 +0100

    second

--- src/main/java/ma/sqli/brute/force/BruteForceApp.java
+++ src/main/java/ma/sqli/brute/force/BruteForceApp.java
@@ -9,14 +9,31 @@ import java.util.LinkedList;
  **/
 public class BruteForceApp {
 
-    private static HashMap<String, Integer> lockedList = new HashMap<>();
+    //tables
+    private static HashMap<String, Integer> lockedListWeb = new HashMap<>();
+    private static HashMap<String, Integer> lockedListAndroid = new HashMap<>();
     private static HashMap<String, String> userList = new HashMap<>();
+    private static HashMap<String, Boolean> blackList = new HashMap<>();
+
+    //messages errors
+    private final String LOGIN_INCORECT = "User or password are incorrect.";
+    private final String ERROR_LOCKED = "Multiple erroneous credentials, your account is locked.";
+    private final String BLACK_LIST = "Your account is blacklisted, contact the CRC to resolve the problem.";
+
+    //attempt of user
+    private int attempt = 0;
 
     public String login(String username, String password) {
-        if (userList.containsKey(username) && userList.get(username) == password) {
+        lockedListWeb.put(username,attempt);
+        if (userList.containsKey(username) && userList.get(username) == password && lockedListWeb.get(username) < 3) {
+            attempt = 0;
             return "Welcome " + username + "!";
-        } else {
-            return "User or password are incorrect.";
+        } else if(userList.containsKey(username) && userList.get(username) != password && lockedListWeb.get(username) < 2){
+            attempt++;
+            lockedListWeb.put(username, attempt);
+            return LOGIN_INCORECT;
+        }else {
+            return ERROR_LOCKED;
         }
     }
 
@@ -27,10 +44,23 @@ public class BruteForceApp {
     }
 
     public String loginWithAndroid(String username, String password) {
-        return "";
+        attempt = 0;
+        lockedListAndroid.put(username,attempt);
+        if (userList.containsKey(username) && userList.get(username) == password && lockedListAndroid.get(username) < 3) {
+            attempt = 0;
+            return "Welcome " + username + "!";
+        } else if(userList.containsKey(username) && userList.get(username) != password && lockedListAndroid.get(username) < 2){
+            attempt++;
+            lockedListAndroid.put(username, attempt);
+            return LOGIN_INCORECT;
+        }else {
+            return ERROR_LOCKED;
+        }
     }
 
     public void blacklist(String sqli) {
-
+        if (userList.containsKey(sqli)) {
+            blackList.put(sqli,true);
+        }
     }
 }
